10993da5ea165a84c0a965a269f2a8d11cfef182
samiuzzaman <chowdhurysamiuzzaman@gmail.com>
2025-01-22 16:27:35 +0600

    Create Booking Secion

---
+++ Assigment-2/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./conponents/Header";
+import Input from "./conponents/Input";
+import Preview from "./conponents/Preview";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      {/* Input Data */}
+      <Input></Input>
+      {/* Preview Data */}
+      <Preview></Preview>
+    </>
+  );
+}
+
+export default App;
